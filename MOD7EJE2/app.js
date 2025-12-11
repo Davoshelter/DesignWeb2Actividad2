5f6148b172304ed4762fd869182b22cad011729a
Davoshelter <gorenadavid7@gmail.com>
2025-12-10 21:31:00 -0400

    CHANGES

---
+++ MOD7EJE2/app.js
@@ -0,0 +1,23 @@
+const toggleSwitch = document.querySelector('#checkbox');
+const currentTheme = localStorage.getItem('theme');
+
+// Verificar preferencia guardada
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+// Función para cambiar tema
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
